0512d7b27a73ba4ff48ad475d415fb27ae7639d8
Shafik-Shafie <shafik.shafie@icloud.com>
2026-01-07 04:05:36 +0800

    index.js

--- index.js
+++ index.js
@@ -4,40 +4,25 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
-// VERIFY TOKEN (MESTI SAMA DENGAN META)
 const VERIFY_TOKEN = "hello_world";
 
-// ================================
-// WEBHOOK VERIFICATION (META)
-// ================================
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token) {
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      console.log("WEBHOOK VERIFIED");
-      return res.status(200).send(challenge);
-    } else {
-      return res.sendStatus(403);
-    }
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
   }
-  return res.sendStatus(400);
 });
 
-// ================================
-// RECEIVE WHATSAPP MESSAGE
-// ================================
 app.post("/webhook", (req, res) => {
-  console.log("Incoming message:");
   console.log(JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
-// ================================
-// START SERVER
-// ================================
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Bot running on port " + PORT);
